a029201347bbda53729e51d6d9524aca38297610
JaceTechDesign <jace@jacetechdesign.com>
2025-02-04 12:02:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def get_random_response():
     return jsonify(random.choice(responses))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000)
