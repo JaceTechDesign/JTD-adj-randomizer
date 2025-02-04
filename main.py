2a0d571afbfd02ab9740580db61db1a1696d9a46
JaceTechDesign <jace@jacetechdesign.com>
2025-02-04 11:20:18 +0100

    initial upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+# Your list of random adjectives
+responses = [
+    "a red", "an orange", "a yellow", "a blue", "a green", "a purple", "a brown",
+    "a black", "a white", "a pink", "a gray", "an indigo", "a turquoise", "a beige",
+    "a teal", "a shitcovered", "a crooked", "a rockhard", "a razor-sharp",
+    "a melted", "a teethmarked", "a smelly", "a dusty", "a slimy", "a dirty",
+    "a blunt", "a jagged", "an unbreakable", "a fart-smelling", "a hateful",
+    "a loving", "an uncivilized", "an unholy", "a delicious", "a tempting",
+    "an attractive", "an ugly", "a vibrating", "a leather", "a sexy", "a hot",
+    "an ice cold"
+]
+
+@app.route('/random', methods=['GET'])
+def get_random_response():
+    return jsonify(random.choice(responses))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
